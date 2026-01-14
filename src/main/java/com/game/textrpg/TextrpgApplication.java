7516b7d303b676491a2b088a180389d8276ab574
s000h99@winitech.com <S000h99@winitech.com>
2026-01-14 16:14:50 +0900

    260114 :tada: : first commit

---
+++ src/main/java/com/game/textrpg/TextrpgApplication.java
@@ -0,0 +1,13 @@
+package com.game.textrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextrpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextrpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/textrpg/TextrpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.textrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextrpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
